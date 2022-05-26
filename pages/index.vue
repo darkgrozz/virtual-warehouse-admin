e95debddcebe447b6cef24e18ecfac782df0d85b
fighantalenta <tatamf.id@gmail.com>
2022-05-27 02:27:16 +0700

    fix : titleling

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center flex-col items-start p-2">
       <span class="text-lg">@2022 Virtual Warehouse Repository</span>
       <div class="flex justify-center mt-2 items-start flex-row">
-        <span>Set-Up By :</span>
+        <span>Set-Up By : </span>
         <a target="blank" href="https://gitlab.com/fighantalenta_"
           >@figantalenta</a
         >
